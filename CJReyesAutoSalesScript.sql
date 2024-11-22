aeca30ddca29a29b5a3df339f9f604cb2ff8a102
sabuharb <abuharb.sandy@gmail.com>
2024-11-22 14:12:20 -0600

    Initial commit

---
+++ CJReyesAutoSalesScript.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS car_dealership;
+CREATE DATABASE car_dealership;
+USE car_dealership;
+
+CREATE TABLE Dealerships (
+DealershipID INTEGER NOT NULL AUTO_INCREMENT,
+Name VARCHAR(50) NOT NULL,
+Address VARCHAR(50) NOT NULL,
+Phone VARCHAR(12) NOT NULL,
+PRIMARY KEY (DealershipID)
+);
